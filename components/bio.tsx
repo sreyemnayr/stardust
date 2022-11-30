f89386bc3336c3f9a58f206af34b89a4fbb0e04b
Ryan Meyers <ryanmeyersweb@gmail.com>
2022-11-30 16:55:04 -0600

    Mobile about

--- components/bio.tsx
+++ components/bio.tsx
@@ -16,9 +16,9 @@ function classNames(...classes: string[]) {
 function Bio({name, title, bio, socials, img, reverse = false, uppercaseName = true} : BioProps) {
   const signedIn = false;
   return (
-    <div className={classNames(!reverse ? "h-[22rem] purple-wave": "-mt-[8rem] -mb-[8rem]", "w-full items-center", "flex")}>
+    <div className={classNames(!reverse ? "h-[22rem] purple-wave bg-left sm:bg-top": "sm:-mt-[8rem] sm:-mb-[8rem]", "w-full items-center", "flex")}>
       <div className={classNames(
-        "info flex w-[48rem] justify-evenly", !reverse ? "h-[16rem]" : "", "items-center mx-auto",
+        "info flex-col sm:flex-row sm:flex sm:w-[48rem] justify-evenly", !reverse ? "h-[16rem]" : "", "items-center mx-auto",
         
       )}>
         <div className={classNames("flex-col w-[16rem]", !reverse ? "order-last " : "order-first ")}></div>
@@ -26,7 +26,7 @@ function Bio({name, title, bio, socials, img, reverse = false, uppercaseName = t
         <div className={classNames(
           "infoText ",
           "items-center flex-col text-center",
-          "w-[12rem]"
+          "sm:w-[12rem]"
         )}>
           <h3 className={classNames(
             !reverse ? "text-romantic-rose" : "text-nostalgia-perfume",
@@ -37,10 +37,10 @@ function Bio({name, title, bio, socials, img, reverse = false, uppercaseName = t
           </h3>
           <p className={classNames(!reverse ? 'text-black' : 'text-moon-honey', 'text-xl leading-snug')}>{title}</p>
         </div>
-        <div className={classNames("flex-col w-[16rem] ", !reverse ? "order-first pl-[4rem]" : "order-last pr-[4rem]")}>
-        <div className="flex items-center justify-center space-x-4 mt-2 h-14 "></div>
+        <div className={classNames("flex-col w-[16rem] ", !reverse ? "order-first sm:pl-[4rem]" : "order-last sm:pr-[4rem]")}>
+        <div className="flex items-center justify-center space-x-4 mt-2 sm:h-14 "></div>
           <img alt={title} src={img} className="h-[12rem] rounded-full mx-auto"/>
-          <div className="flex items-center justify-center space-x-4 mt-1 h-14">
+          <div className="hidden sm:flex items-center justify-center space-x-4 mt-1 h-14">
             {socials?.map((s) => (
               <SocialIcon key={`${name} ${s}`} url={s} bgColor="currentColor" fgColor={colors['moon-honey']} style={{height: "2rem", width: "2rem"}} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange" />
             ))}
--- components/footer.tsx
+++ components/footer.tsx
@@ -9,14 +9,14 @@ function Footer() {
   
   return (
     <footer className="h-72 pt-32 px-5 flex justify-between items-center">
-        <img src="/img/logo.png" className="h-32" alt="Stardust Society" />
-        <img src="/img/madeofstardust.png" className="h-14 mt-6" alt="We are all made of stardust" />
+        <img src="/img/logo.png" className="hidden sm:block h-32" alt="Stardust Society" />
+        <img src="/img/madeofstardust.png" className="hidden sm:block h-14 mt-6" alt="We are all made of stardust" />
         <div className="h-10 flex space-x-4">
           
-          <SocialIcon url="https://twitter.com/stardust7inc" bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange" />
-          <SocialIcon url="https://instagram.com/stardust7inc" bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
-          <SocialIcon url="http://discord.gg/stardustsociety" bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
-          <SocialIcon url="https://opensea.io/collection/stardustgeneration" defaultSVG={OpenSeaIcon} bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
+          <SocialIcon url="https://twitter.com/stardust7inc" style={{height: "2rem", width: "2rem"}} bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange" />
+          <SocialIcon url="https://instagram.com/stardust7inc" style={{height: "2rem", width: "2rem"}} bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
+          <SocialIcon url="http://discord.gg/stardustsociety" style={{height: "2rem", width: "2rem"}} bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
+          <SocialIcon url="https://opensea.io/collection/stardustgeneration" style={{height: "2rem", width: "2rem"}} defaultSVG={OpenSeaIcon} bgColor="currentColor" fgColor={colors['moon-honey']} className="text-romantic-rose rounded-full hover:ring-2 hover:ring-moon-honey hover:text-marquis-orange"  />
           
         </div>
     </footer>
