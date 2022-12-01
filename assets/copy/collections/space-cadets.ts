455ce84d2851817a3a72e2d313e0224a957a4f76
Ryan Meyers <ryanmeyersweb@gmail.com>
2022-12-01 14:45:06 -0600

    tempp files to make build work

---
+++ assets/copy/collections/space-cadets.ts
@@ -0,0 +1,11 @@
+export const copy = {
+  hero: {
+    
+    title: "Stardust Generation",
+    subtitle: "Is the first generative collection from Stardust Society of 7777 unique dreamers!",
+    button: {
+      title: "Mint a Dreamer",
+      href: "mint",
+    }
+  },
+}
